aa4b155de0d54e8ffc96b4acc64172a33785e768
Shalitha Suranga <shalithasuranga@gmail.com>
2021-04-23 18:31:08 +0530

    Add new required attributes to the config file

---
+++ .lite_workspace.lua
@@ -0,0 +1,1 @@
+return {["active_view"]=2,["type"]="leaf",["views"]={[1]={["active"]=false,["type"]="view",["module"]="core.logview"},[2]={["filename"]="neutralino.config.json",["text"]=false,["active"]=true,["type"]="doc",["scroll"]={["y"]=0,["x"]=0},["selection"]={[1]=6,[2]=29,[3]=6,[4]=29}}}}
