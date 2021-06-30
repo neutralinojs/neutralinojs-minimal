a244161b972cda4fa12883f22391fcea52ac4749
Shalitha Suranga <shalithasuranga@gmail.com>
2021-06-30 20:02:17 +0530

    #5: Add a simple tray menu and some features for devs to get started

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,15 +1,51 @@
 
 window.myApp = {
     showInfo: () => {
-        document.getElementById('info').innerHTML = NL_APPID + " is running on port " +
-                    NL_PORT + " inside " + NL_OS + ".<br/><br/>" + "<span>v" + NL_VERSION + "</span>";
+        document.getElementById('info').innerHTML = `
+            ${NL_APPID} is running on port ${NL_PORT}  inside ${NL_OS} 
+            <br/><br/>
+            <span>server: v${NL_VERSION} | client: v${NL_CVERSION}</span>
+            `;
     },
     openDocs: () => {
         Neutralino.app.open({
             url: "https://neutralino.js.org/docs"
         });
+    },
+    setTray: () => {
+        if(NL_MODE != "window") {
+            console.log("INFO: Tray menu is only available in the window mode.");
+            return;
+        }
+        let tray = {
+            icon: "/resources/icons/trayIcon.png",
+            menuItems: [
+                {id: "VERSION", text: "Get version"},
+                {id: "SEP", text: "-"},
+                {id: "QUIT", text: "Quit"}
+            ]
+        };
+        Neutralino.os.setTray(tray);
+    }
+};
+
+Neutralino.events = {
+    onTrayMenuItemClicked: (menuItem) => {
+        switch(menuItem.id) {
+            case "VERSION":
+                Neutralino.os.showMessageBox({
+                    type: "INFO",
+                    title: "Version information",
+                    content: `Neutralinojs server: v${NL_VERSION} | Neutralinojs client: v${NL_CVERSION}` 
+                });
+                break;
+            case "QUIT":
+                Neutralino.app.exit();
+                break;
+        }
     }
 };
 
 Neutralino.init();
+window.myApp.setTray();
 window.myApp.showInfo();
