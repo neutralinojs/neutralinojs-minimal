0a2511ccf0d78ed35c8eca4ced00876371feedee
Shalitha Suranga <shalithasuranga@gmail.com>
2021-12-11 18:57:48 +0530

    Update main.js

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,60 +1,62 @@
+// This is just a sample app. You can structure your Neutralinojs app code as you wish.
+// This example app is written with vanilla JavaScript and HTML.
+// Fee free to use any frontend framework you like :)
 
-window.myApp = {
-    showInfo: () => {
-        document.getElementById('info').innerHTML = `
-            ${NL_APPID} is running on port ${NL_PORT}  inside ${NL_OS} 
-            <br/><br/>
-            <span>server: v${NL_VERSION} . client: v${NL_CVERSION}</span>
-            `;
-    },
-    openDocs: () => {
-        Neutralino.os.open("https://neutralino.js.org/docs");
-    },
-    openTutorial: () => {
-        Neutralino.os.open("https://www.youtube.com/watch?v=txDlNNsgSh8&list=PLvTbqpiPhQRb2xNQlwMs0uVV0IN8N-pKj");
-    },
-    setTray: () => {
-        if(NL_MODE != "window") {
-            console.log("INFO: Tray menu is only available in the window mode.");
-            return;
-        }
-        let tray = {
-            icon: "/resources/icons/trayIcon.png",
-            menuItems: [
-                {id: "VERSION", text: "Get version"},
-                {id: "SEP", text: "-"},
-                {id: "QUIT", text: "Quit"}
-            ]
-        };
-        Neutralino.os.setTray(tray);
-    },
-    onTrayMenuItemClicked: (event) => {
-        switch(event.detail.id) {
-            case "VERSION":
-                Neutralino.os.showMessageBox("Version information",
-                    `Neutralinojs server: v${NL_VERSION} | Neutralinojs client: v${NL_CVERSION}`);
-                break;
-            case "QUIT":
-                Neutralino.app.exit();
-                break;
-        }
-    },
-    onWindowClose: () => {
-        Neutralino.app.exit();
+function showInfo() {
+    document.getElementById('info').innerHTML = `
+        ${NL_APPID} is running on port ${NL_PORT}  inside ${NL_OS}
+        <br/><br/>
+        <span>server: v${NL_VERSION} . client: v${NL_CVERSION}</span>
+        `;
+}
+
+function openDocs() {
+    Neutralino.os.open("https://neutralino.js.org/docs");
+}
+
+function openTutorial() {
+    Neutralino.os.open("https://www.youtube.com/watch?v=txDlNNsgSh8&list=PLvTbqpiPhQRb2xNQlwMs0uVV0IN8N-pKj");
+}
+
+function setTray() {
+    if(NL_MODE != "window") {
+        console.log("INFO: Tray menu is only available in the window mode.");
+        return;
     }
-};
+    let tray = {
+        icon: "/resources/icons/trayIcon.png",
+        menuItems: [
+            {id: "VERSION", text: "Get version"},
+            {id: "SEP", text: "-"},
+            {id: "QUIT", text: "Quit"}
+        ]
+    };
+    Neutralino.os.setTray(tray);
+}
 
-// Initialize native API communication. This is non-blocking
-// use 'ready' event to run code on app load.
-// Avoid calling API functions before init or after init.
-Neutralino.init(); 
-
-Neutralino.events.on("trayMenuItemClicked", myApp.onTrayMenuItemClicked);
-Neutralino.events.on("windowClose", myApp.onWindowClose);
-Neutralino.events.on("ready", () => {
-    if(NL_OS != "Darwin") { // TODO: Fix https://github.com/neutralinojs/neutralinojs/issues/615
-        window.myApp.setTray();
+function onTrayMenuItemClicked(event) {
+    switch(event.detail.id) {
+        case "VERSION":
+            Neutralino.os.showMessageBox("Version information",
+                `Neutralinojs server: v${NL_VERSION} | Neutralinojs client: v${NL_CVERSION}`);
+            break;
+        case "QUIT":
+            Neutralino.app.exit();
+            break;
     }
-})
+}
 
-window.myApp.showInfo();
+function onWindowClose() {
+    Neutralino.app.exit();
+}
+
+Neutralino.init();
+
+Neutralino.events.on("trayMenuItemClicked", onTrayMenuItemClicked);
+Neutralino.events.on("windowClose", onWindowClose);
+
+if(NL_OS != "Darwin") { // TODO: Fix https://github.com/neutralinojs/neutralinojs/issues/615
+    setTray();
+}
+
+showInfo();
